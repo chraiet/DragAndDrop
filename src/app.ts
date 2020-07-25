e3a24ac8a9e5c74cb64f8147277b6b165248e316
Momo <chraiet009@outlook.com>
2020-07-25 11:53:25 -0400

    Added Project class and Listener type

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,25 @@
 
+enum ProjectStatus {
+    Active,
+    Finished
+}
+
+class Project {
+    constructor(
+        public id: string,
+        public title: string,
+        public description: string,
+        public people: number,
+        public status: ProjectStatus
+    ) {}
+}
+
+type Listener = (items: Project[]) => void
+
 //  Project State Management
 class ProjectState {
-    private listeners: any[] = []
-    private projects: any[] = []
+    private listeners: Listener[] = []
+    private projects: Project[] = []
     private static instance: ProjectState
 
     private constructor() {}
@@ -16,17 +33,15 @@ class ProjectState {
         return this.instance
     }
 
-    addListener(listenerFn: Function) {
+    addListener(listenerFn: Listener) {
         this.listeners.push(listenerFn)
     }
 
     addProject(title: string, description: string, numOfPeople: number) {
-        const newProject = {
-            id: Math.random().toString(),
-            title,
-            description,
-            numOfPeople
-        }
+        const id = Math.random().toString()
+        const newProject = new Project(
+            id, title, description, numOfPeople, ProjectStatus.Active
+        )
 
         this.projects.push(newProject)
         
@@ -92,7 +107,7 @@ class ProjectList {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
     element: HTMLElement
-    assignedProjects: any[]
+    assignedProjects: Project[]
 
     constructor(private type: 'active' | 'finished') {
         this.templateElement = document.getElementById('project-list') as HTMLTemplateElement
@@ -103,7 +118,7 @@ class ProjectList {
         this.element = importedNode.firstElementChild as HTMLElement
         this.element.id = `${this.type}-projects`
 
-        projectState.addListener((projects: any[]) => {
+        projectState.addListener((projects: Project[]) => {
             this.assignedProjects = projects
             this.renderProjects()
         })
