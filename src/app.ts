5877f1a29a0b6b5d888a5cb34745a9406db22146
Momo <chraiet009@outlook.com>
2020-07-25 12:08:42 -0400

    Fixes

--- src/app.ts
+++ src/app.ts
@@ -119,7 +119,16 @@ class ProjectList {
         this.element.id = `${this.type}-projects`
 
         projectState.addListener((projects: Project[]) => {
-            this.assignedProjects = projects
+            const relevantProjects = projects.filter(
+                project => {
+                    if (type === 'active') {
+                        return project.status === ProjectStatus.Active
+                    }
+                    return project.status === ProjectStatus.Finished
+                }
+            )
+
+            this.assignedProjects = relevantProjects
             this.renderProjects()
         })
 
@@ -129,6 +138,8 @@ class ProjectList {
 
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`) as HTMLUListElement
+
+        listEl.innerHTML = ''
         for (const prjItem of this.assignedProjects) {
             const listItem = document.createElement('li')
             listItem.textContent = prjItem.title
